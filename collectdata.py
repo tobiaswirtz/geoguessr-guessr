4751871362db5f38cbae775f00385802ed3b5384
Tobias Wirtz <tobias.wirtz1@gmail.com>
2021-07-13 18:49:14 +0200

    Test

--- collectdata.py
+++ collectdata.py
@@ -1,5 +1,23 @@
 from PIL import ImageGrab
 from pynput import keyboard 
+import numpy as np
+import cv2
+
+actual_location_marker_img = cv2.imread('assets/marker.png', 0)
+h_marker, w_marker = actual_location_marker_img.shape
+
+def get_coordinates_of_marker(input_img):
+    img2 = input_img.copy()
+    print(actual_location_marker_img.dtype, img2.dtype, actual_location_marker_img.shape, img2.shape)
+    result = cv2.matchTemplate(img2, actual_location_marker_img.astype(np.uint8), cv2.TM_CCOEFF)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    location = max_loc
+    bottom_right = (location[0] + w_marker, location[1] + h_marker)
+    cv2.rectangle(img2, location, bottom_right, 255, 5)
+    cv2.imwrite('Match.png', img2)
+    cv2.waitKey(5000)
+    cv2.destroyAllWindows()
+    return (0, 1)
 
 
 def save_image():
@@ -8,7 +26,10 @@ def save_image():
     im.show()
 
 def save_coordinates():
-    print("Saving coordinates")
+    tmp_img = ImageGrab.grab(bbox=(25, 400, 2750, 1200), xdisplay=None)
+    numpy_tmp = np.array(tmp_img)
+    opencv_img = cv2.cvtColor(numpy_tmp, cv2.COLOR_RGB2GRAY)
+    coords = get_coordinates_of_marker(opencv_img.astype(np.uint8))
 
 with keyboard.GlobalHotKeys({
         '<cmd_l>+s': save_image,
